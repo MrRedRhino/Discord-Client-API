16f119fba285d884486ffa748b4c5aee2735518b
pipeman <80647470+MrRedRhino@users.noreply.github.com>
2022-12-29 16:37:29 +0100

    Cleanup imports

--- src/main/java/org/pipeman/dccapi/events/messages/MessageCreateEvent.java
+++ src/main/java/org/pipeman/dccapi/events/messages/MessageCreateEvent.java
@@ -3,7 +3,6 @@ package org.pipeman.dccapi.events.messages;
 import org.json.JSONObject;
 import org.pipeman.dccapi.events.DiscordEvent;
 import org.pipeman.dccapi.wrappers.Message;
-import org.pipeman.dccapi.wrappers.MessageAuthor;
 
 public record MessageCreateEvent(Message message) implements DiscordEvent {
     public static MessageCreateEvent from(JSONObject d) {
--- src/main/java/org/pipeman/dccapi/events/messages/MessageEditEvent.java
+++ src/main/java/org/pipeman/dccapi/events/messages/MessageEditEvent.java
@@ -2,8 +2,6 @@ package org.pipeman.dccapi.events.messages;
 
 import org.json.JSONObject;
 import org.pipeman.dccapi.events.DiscordEvent;
-import org.pipeman.dccapi.wrappers.Message;
-import org.pipeman.dccapi.wrappers.MessageAuthor;
 import org.pipeman.dccapi.wrappers.MessageSnapshot;
 
 public record MessageEditEvent(MessageSnapshot newMessage) implements DiscordEvent {
